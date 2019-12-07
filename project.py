2eaa376dd1491b09ccc047ba1f90a5b13a28a64d
mease <derek.mease@colorado.edu>
2019-12-06 21:42:15 -0700

    prediction maps from literature

--- project.py
+++ project.py
@@ -13,7 +13,7 @@ artic_polys = list(artic_poly.geometries())
 artic_polys = cascaded_union(artic_polys)
 ocean_polys = cascaded_union(list(ocean_poly.geometries()))
 
-def plt_ant_feat(z, x=None, y=None, fig=None, ax=None, cmap='jet', vmin=20, vmax=135, hide_water=True, label='', title='', kwargs_mesh={'shading':'gouraud', 'aa':True}, kwargs_cbar={}):
+def plt_ant_feat(z, x=None, y=None, fig=None, ax=None, cmap='jet', vmin=20, vmax=135, hide_water=True, ups=False,  label='', title='', kwargs_mesh={'shading':'gouraud', 'aa':True}, kwargs_cbar={}):
     if x is None or y is None:
         x = np.linspace(-179.5, 179.5, 360)
         y = np.linspace(-60.5, -89.5, 30)
@@ -31,8 +31,11 @@ def plt_ant_feat(z, x=None, y=None, fig=None, ax=None, cmap='jet', vmin=20, vmax
     ax.add_feature(cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '10m'),facecolor='none', edgecolor='black')
     if hide_water:
         ax.add_geometries(ocean_polys.symmetric_difference(artic_polys), ccrs.PlateCarree(), facecolor='w', edgecolor=None)
-        ax.add_feature(cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '10m'), facecolor='none', edgecolor='black', lw=1)    
-    mesh = ax.pcolormesh(x, y, z, cmap=cmap, vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree(), **kwargs_mesh)
+        ax.add_feature(cartopy.feature.NaturalEarthFeature('physical', 'antarctic_ice_shelves_polys', '10m'), facecolor='none', edgecolor='black', lw=1)
+    if ups:
+        mesh = ax.scatter(x, y, c=z, cmap=cmap, vmin=vmin, vmax=vmax)
+    else:
+        mesh = ax.pcolormesh(x, y, z, cmap=cmap, vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree(), **kwargs_mesh)
     cb = plt.colorbar(mesh, ax=ax, **kwargs_cbar)
     cb.set_label(label)
     ax.set_title(title)
